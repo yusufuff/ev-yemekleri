ac2f38e9222e54cfea7e83683c150408fa8849b1
yusufuff <yusufagal06@gmail.com>
2026-03-11 13:03:31 +0300

    page.tsx onarildi

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+﻿import Link from `"next/link`"
+export default function HomePage() {
+  return (
+    <div>
+      <nav>
+        <Link href="/">EV YEMEKLERİ</Link>
+        <Link href="/giris">Giriş Yap</Link>
+      </nav>
+      <h1>Mahallendeki En İyi Ev Yemekleri</h1>
+      <Link href="/kesif">Keşfet</Link>
+    </div>
+  )
+}
