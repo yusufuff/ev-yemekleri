bd045d48adbd3b203bf8e56592e3f61e0d2f721b
yusufuff <yusufagal06@gmail.com>
2026-04-01 00:01:59 +0300

    fix: discover force-dynamic eklendi

--- app/api/discover/route.ts
+++ app/api/discover/route.ts
@@ -1,7 +1,7 @@
 ﻿// @ts-nocheck
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
-
+export const dynamic = 'force-dynamic'
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
