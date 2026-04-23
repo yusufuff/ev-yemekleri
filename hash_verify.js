8a2e0ec8f69faf47483cd8b100f074c0b9b1ac92
yusufuff <yusufagal06@gmail.com>
2026-04-23 11:51:42 +0300

    feat: gizlilik politikasi sayfasi eklendi

--- hash_verify.js
+++ hash_verify.js
@@ -1,1 +1,1 @@
-const b = require("bcryptjs"); b.compare("AnneElim2026!", "$2b$10$d4awd1HuDvnFd6mZG69G1.c/0gvjXGI7DSl16fTwxdJURy/Tk7NDG", function(err, r) { console.log("Eslesme:", r); });
+const b = require("bcryptjs"); b.compare("201500", "$2b$10$vDJDB2FHYn1SREBy2.rv9O./qQr94PdBiQuU2PsRs/oUg7dZqgKWm", function(err, r) { console.log("Eslesme:", r); });
